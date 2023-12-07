b3eb258ef3e0882db27a14dd62c05ee61e857bdb
Nazawrius <Nazawrius@gmail.com>
2023-12-08 01:52:09 +0200

    Completed Wiener

--- Wiener.py
+++ Wiener.py
@@ -4,21 +4,61 @@ from random import gauss
 import numpy as np
 import matplotlib.pyplot as plt
 
+def gauss_list(M):
+    return [gauss(0, 1) for _ in range(M+1)]
+
+def t(dt):
+    t = []
+    s = 0
+    while s <= 1:
+        t.append(s)
+        s += dt
+    return t
+
 def gen_process(M, dt):
     T = []
-    for j in range(floor(1/dt) + 1):
-        t = j * dt
-        w = t*gauss(0, 1) + 2**0.5 * sum(sin(i*pi*t) / (i*pi) * gauss(0, 1) for i in range(1, M+1))
+    t = 0
+    gl = gauss_list(M)
+    while t <= 1:
+        w = t*gl[0] + 2**0.5 * sum(sin(i*pi*t) / (i*pi) * gl[i] for i in range(1, M+1))
         T.append(w)
+        t += dt
     
     return T
 
-def plot_processes(processes, dt):
-    t = [i*dt for i in range(floor(1/dt) + 1)]
+def plot_processes(processes, dt):    
     for process in processes:
-        plt.plot(t, process, alpha=0.4)
+        plt.plot(t(dt), process, alpha=0.4)
     
     plt.show()
 
 def approx_mean_var(process):
-    return np.mean(process), np.mean(process)
+    return np.mean(process), np.var(process)
+
+def plot_mean_var(processes):
+    means_and_vars = [approx_mean_var(process) for process in processes]
+    means = [m for (m, _) in means_and_vars]
+    vars = [v for (_, v) in means_and_vars]
+    plt.hist(means, rwidth=0.7)
+    plt.title("Means")
+    plt.show()
+
+    plt.hist(vars, rwidth=0.7)
+    plt.title("Vars")
+    plt.show()
+
+    print("Global mean:", sum(means) / len(means))
+    print("Mean variance:", sum(vars) / len(vars))
+
+def plot_first_exit(processes, alpha, dt):
+    first_exit = []
+    times = t(dt)
+    
+    for process in processes:
+        for (i, w) in enumerate(process):
+            if w >= alpha:
+                first_exit.append(times[i])
+    
+    plt.hist(first_exit, bins=20)
+    plt.title("First exit")
+    plt.show()
--- main_Wiener.py
+++ main_Wiener.py
@@ -1,15 +1,19 @@
-from Wiener import gen_process, plot_processes
+from Wiener import gen_process, plot_processes, plot_mean_var, plot_first_exit
 
 def main_Wiener():
     delta = 0.1
     eps = 0.05
     M = 10000
-    dt = 0.01
+    dt = 0.005
 
     processes = [gen_process(M, dt) for _ in range(4)]
     plot_processes(processes, dt)
 
-    many_processes = [gen_process(M, dt) for _ in range(250)]
+    many_processes = [gen_process(M, dt) for _ in range(150)]
+    plot_mean_var(many_processes)
+
+    alpha = 0.4
+    plot_first_exit(many_processes, alpha, dt)
 
 if __name__ == "__main__":
     main_Wiener()
