3927b0a569094584f978e38b8892da0086d2ae8f
Nazawrius <Nazawrius@gmail.com>
2023-12-07 21:49:45 +0200

    Added plotting of processes

--- Poisson.py
+++ Poisson.py
@@ -1,14 +1,25 @@
 from math import log1p
 from random import random
 
+import matplotlib.pyplot as plt
+
 def gen_process(intensity, time):
     events = []
     
     t = 0
     while t < time:
-        event = -log1p(-random()) / intensity
-        t += event
+        step = -log1p(-random()) / intensity
+        t += step
         if t < time:
-            events.append(event)
+            events.append(t)
     
     return events
+
+def plot_processes(processes):
+    for (i, events) in enumerate(processes):
+        plt.step(events, range(len(events), label=f"Realization #{i}"))
+    
+    plt.xlabel("Time")
+    plt.ylabel("Number of events")
+    plt.legend()
+    plt.show()
