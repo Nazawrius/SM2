f9d846b12d33d0473e452ee2ce7b43db89eabdf0
Nazawrius <Nazawrius@gmail.com>
2023-12-08 03:34:07 +0200

    Completed absorbtion Markov

--- Markov.py
+++ Markov.py
@@ -1,5 +1,7 @@
 from random import random
 
+import matplotlib.pyplot as plt
+
 def choice(v):
     x = random()
     i = 0
@@ -13,8 +15,48 @@ def path(matrix, vector):
     state = choice(vector)
     states = [state]
 
-    while state != len(matrix):
+    while state != len(matrix) - 1:
         state = choice(matrix[state])
         states.append(state)
     
-    return states
+    return states + [state]
+
+def plot_paths(paths):
+    for path in paths:
+        n = list(range(len(path)))
+        plt.plot(n, path)
+        plt.show()
+
+def experimental_matrix(paths):
+    exp_m = [[[0, 0] for _ in range(5)] for j in range(5)]
+
+    for path in paths:
+        for i in range(len(path) - 1):
+            for j in range(5):
+                exp_m[path[i]][j][1] += 1
+            exp_m[path[i]][path[i+1]][0] += 1
+        
+    for i in range(5):
+        for j in range(5):
+            exp_m[i][j] = round(exp_m[i][j][0] / exp_m[i][j][1], 2) if exp_m[i][j][1] != 0 else 0.0
+    
+    return exp_m
+
+def experimental_consecutive_time(paths):
+    exp_t = [[0, 0] for _ in range(5)]
+    for path in paths:
+        for i in range(5):
+            exp_t[i][0] += path.count(i)
+        
+        for i in range(len(path) - 1):
+            if path[i+1] != path[i]:
+                exp_t[path[i]][1] += 1
+        exp_t[path[-1]][1] += 1    
+    
+    for i in range(5):
+        exp_t[i] = round(exp_t[i][0] / exp_t[i][1], 2)
+    
+    return exp_t
+
+def mean_absorbsion_time(paths):
+    return sum(len(path) - 2 for path in paths) / len(paths)
--- main_Markov.py
+++ main_Markov.py
@@ -1,14 +1,24 @@
-from Markov import choose
+from Markov import path, plot_paths, experimental_matrix, experimental_consecutive_time, mean_absorbsion_time
 
 def main_Markov():
     matrix = [
         [0.4, 0.5, 0.0, 0.1, 0.0],
-        [0.0, 0.1, 0.1, 0.1, 0.7],
-        [0.3, 0.0, 0.0, 0.3, 0.4],
+        [0.1, 0.1, 0.6, 0.1, 0.1],
+        [0.4, 0.2, 0.0, 0.3, 0.1],
         [0.5, 0.0, 0.2, 0.2, 0.1],
         [0.0, 0.0, 0.0, 0.0, 1.0]
     ]
-    vector = [0.3, 0.1, 0.2, 0.2, 0.2]
+    vector = [0.3, 0.1, 0.4, 0.2, 0.0]
+
+    paths = [path(matrix, vector) for _ in range(300)]
+    plot_paths(paths[:4])
+    exp_m = experimental_matrix(paths)
+    for row in exp_m:
+        print(' '.join(map(str, row)))
+    
+    print(experimental_consecutive_time(paths))
+
+    print(mean_absorbsion_time(paths))
 
 if __name__ == "__main__":
     main_Markov()
